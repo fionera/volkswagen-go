6375e54fba7bb845249bd7737ed99512bce0a037
Fionera <fionera@fionera.de>
2022-11-29 00:42:46 +0100

    only trigger if in CI

--- volkswagen.go
+++ volkswagen.go
@@ -1,6 +1,7 @@
 package volkswagen_go
 
 import (
+	"os"
 	"reflect"
 	"testing"
 	_ "unsafe"
@@ -8,7 +9,30 @@ import (
 	"bou.ke/monkey"
 )
 
+var envVars = []string{
+	"BUILD_ID",               // Jenkins, Cloudbees
+	"BUILD_NUMBER",           // Jenkins, TeamCity
+	"CI",                     // Travis CI, CircleCI, Cirrus CI, Gitlab CI, Appveyor, CodeShip, dsari
+	"CI_APP_ID",              // Appflow
+	"CI_BUILD_ID",            // Appflow
+	"CI_BUILD_NUMBER",        // Appflow
+	"CI_NAME",                // Codeship and others
+	"CONTINUOUS_INTEGRATION", // Travis CI, Cirrus CI
+	"RUN_ID",                 // TaskCluster, dsari
+}
+
 func init() {
+	var isCI bool
+	for _, s := range envVars {
+		if _, set := os.LookupEnv(s); set {
+			isCI = true
+		}
+	}
+
+	if !isCI {
+		return
+	}
+
 	f, _ := reflect.TypeOf(testing.T{}).FieldByName("common")
 	monkey.PatchInstanceMethodIgnoreType(reflect.PointerTo(f.Type), "Fail", func() {
 	})
